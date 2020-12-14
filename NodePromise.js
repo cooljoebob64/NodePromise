2ef02eb1ea62b3408d2d537f9018172d3f637436
Joshua Luppes <cooljoebob64@gmail.com>
2020-12-14 15:04:40 -0600

    Updated to more standard promise notation

--- NodePromise.js
+++ NodePromise.js
@@ -1,40 +1,48 @@
 const { readFile } = require("fs");
 const https = require("https");
 
-async function yearOfMovies(year) {
-  yourList = await getMovies(year);
+async function printYearOfMovies(year) {
+  let yourList;
+  await getMovies(year).then(
+    (res) => {
+      yourList = res;
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
   console.log(yourList);
 }
 
 function getMovies(year) {
-   
-    myPromise = new Promise((resolve, reject) => {
-
+  myPromise = new Promise((resolve, reject) => {
     const url = `https://jsonmock.hackerrank.com/api/movies?Year=${year}`;
+    // const url = "https://notarealurl.oopsies.definitelynotawebpage"; // this will cause the GET call to fail! for testing purposes.
     let movieData = https
       .get(url, (resp) => {
         let data = "";
         let movieList = [];
 
         resp.on("data", (chunk) => {
-          data += chunk;
+          data += chunk; //add the chunks together to make one big data
         });
 
         resp.on("end", () => {
           movieList = JSON.parse(data).data;
-        //   console.log(movieList);  //debug log
-          resolve( movieList);
+          //   console.log(movieList);  //logs the movie list for debugging
+          resolve(movieList);
         });
       })
       .on("error", (err) => {
-        console.log("Error: " + err.message);
+        reject(err);
       });
 
-      return movieData;
+    return movieData;
   });
 
   return myPromise;
 }
 
-yearOfMovies(2012);
-yearOfMovies(2001);
+printYearOfMovies(2012);
+// printYearOfMovies(2001);
+// printYearOfMovies(1805); //returns an empty array 
